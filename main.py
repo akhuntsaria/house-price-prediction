2c744aa728165d862a41bb3bf98bf14c7b09fb80
Akaki Khuntsaria <49805336+akhuntsaria@users.noreply.github.com>
2021-09-09 22:49:46 +0400

    Add manual test; Refactor

--- main.py
+++ main.py
@@ -2,18 +2,34 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("data.csv")
+def get_linear_regression_and_test_data():
+    df = pd.read_csv("data.csv")
 
-X = df[['sqm']]
-y = df['price']
+    X = df[['sqm']]
+    y = df['price']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-reg = LinearRegression()
-reg.fit(X_train, y_train)
+    reg = LinearRegression()
+    reg.fit(X_train, y_train)
 
-print("------- Automatic test -------")
-print("Prediction:", reg.predict(X_test))
-print("Actual:", y_test.values)
-print("Score", reg.score(X_test, y_test))
-print("------------------------------\n")
+    return reg, X_test, y_test
+
+def run_automatic_test(reg, X_test, y_test):
+    print("------- Automatic test -------")
+    print("Test values:\n", X_test.values)
+    print("Prediction:", reg.predict(X_test))
+    print("Actual:", y_test.values)
+    print("Score", reg.score(X_test, y_test), "\n")
+
+def run_manual_test(reg):
+    print("------- Manual test -------")
+    sqmInput = float(input('Enter sqm: '))
+
+    # Convert input to 2D array and get prediction
+    print("Prediction:", reg.predict([[sqmInput]])[0])
+
+reg, X_test, y_test = get_linear_regression_and_test_data()
+
+run_automatic_test(reg, X_test, y_test)
+run_manual_test(reg)
