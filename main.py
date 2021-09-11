4eca64aaaf0587f8435443f13a81a3c1a6686d06
Akaki Khuntsaria <akakikhuntsaria97@gmail.com>
2021-09-11 17:19:56 +0400

    Add a parameter

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 def get_linear_regression_and_test_data():
     df = pd.read_csv("data.csv")
 
-    X = df[['sqm']]
+    X = df[['sqm', 'rooms']]
     y = df['price']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -24,15 +24,21 @@ def run_automatic_test(reg, X_test, y_test):
     print("Score", reg.score(X_test, y_test), "\n")
 
 def run_manual_test(reg):
-    print("------- Manual test -------")
-    sqmInput = input('Enter sqm (or q!): ')
+    print("------- Manual test -------\nEnter 'q!' to quit\n")
+
+    sqmInput = input('Enter sqm: ')
     if sqmInput == 'q!':
         sys.exit()
 
+    rooms = input('Enter number of rooms: ')
+    if rooms == 'q!':
+        sys.exit()
+
     sqmInput = float(sqmInput)
+    rooms = int(rooms)
 
     # Convert input to 2D array and get prediction
-    print("Prediction:", reg.predict([[sqmInput]])[0])
+    print("Prediction:", reg.predict([[sqmInput, rooms]])[0], "\n")
 
 reg, X_test, y_test = get_linear_regression_and_test_data()
 
